4141f5ed8e6dc9dae379a578266561b05e7d3dc9
Philip Engler <philip.engler@gmx.de>
2018-09-11 18:33:59 +0200

    Backup

---
+++ UmaUma Backup/Assets/Scripts/WorldMap.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour {
+
+	public Terrain WorldTerrain;
+	//public Terrain Water;
+	public LayerMask TerrainLayer;
+	//public LayerMask WaterLayer;
+	public static float TerrainLeft, TerrainRight, TerrainBottom, TerrainWidth, TerrainLength, TerrainHeight, TerrainTop;
+
+	public static ArrayList units = new ArrayList();
+	public static ArrayList positions = new ArrayList();
+	public static ArrayList rotations = new ArrayList();
+
+	public void Awake() {
+
+		TerrainLeft = WorldTerrain.transform.position.x;
+		TerrainBottom = WorldTerrain.transform.position.z;
+		TerrainWidth = WorldTerrain.terrainData.size.x;
+		TerrainLength = WorldTerrain.terrainData.size.z;
+		TerrainHeight = WorldTerrain.terrainData.size.y;
+		TerrainRight = TerrainLeft + TerrainWidth;
+		TerrainTop = TerrainBottom + TerrainLength;
+
+		InstantiateRandomPosition("Prefabs/Bad_Coconut", 700, 0.5f);
+		InstantiateRandomPosition("Prefabs/Cactus_Tall", 2000, 0f);
+		InstantiateRandomPosition("Prefabs/Good_Coconut", 500, 0.5f);
+		
+	}
+
+
+	public void InstantiateRandomPosition(string Resource, int Amount, float AddedHeight) {
+
+		var i = 0;
+		float terrainHeight = 0f;
+		RaycastHit hit;
+		float randomPositionX, randomPositionY, randomPositionZ;
+		Vector3 randomPosition = Vector3.zero;
+
+		do {
+			i++;
+			randomPositionX = Random.Range (TerrainLeft, TerrainRight);
+			randomPositionZ = Random.Range (TerrainBottom, TerrainTop);
+
+			if (Physics.Raycast(new Vector3(randomPositionX, 9999f, randomPositionZ), Vector3.down, out hit, Mathf.Infinity, TerrainLayer)) {
+				terrainHeight = hit.point.y;
+			}
+
+			if (hit.point.y < 1) {
+				terrainHeight = -100;
+			}
+
+			randomPositionY = terrainHeight + AddedHeight;
+
+			randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+
+			Instantiate(Resources.Load (Resource, typeof(GameObject)), randomPosition, Quaternion.identity);
+
+
+		} while (i < Amount);
+
+
+
+	}
+}
