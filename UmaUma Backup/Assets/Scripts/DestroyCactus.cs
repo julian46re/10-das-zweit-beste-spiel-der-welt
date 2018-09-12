04d629835de7c35a1029a6d962912031d7bdda97
Philip Engler <philip.engler@gmx.de>
2018-09-12 21:42:19 +0200

    Revert "update"
    This reverts commit 1c6ee8d98e593363d22c2689b6749c3365c5b87e.

--- UmaUma Backup/Assets/Scripts/DestroyCactus.cs
+++ UmaUma Backup/Assets/Scripts/DestroyCactus.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class DestroyCactus : MonoBehaviour {
     //Variables
 
-    //public Terrain WorldTerrain;
-    //public LayerMask TerrainLayer;
+    public Terrain WorldTerrain;
+    public LayerMask TerrainLayer;
     public static float TerrainLeft, TerrainRight, TerrainBottom, TerrainWidth, TerrainLength, TerrainHeight, TerrainTop;
 
     public int cactusHealth = 5;
@@ -18,13 +18,13 @@ public class DestroyCactus : MonoBehaviour {
     private void Start() {
     	cactus = transform.gameObject;
 
-        // TerrainLeft = WorldTerrain.transform.position.x;
-        // TerrainBottom = WorldTerrain.transform.position.z;
-        // TerrainWidth = WorldTerrain.terrainData.size.x;
-        // TerrainLength = WorldTerrain.terrainData.size.z;
-        // TerrainHeight = WorldTerrain.terrainData.size.y;
-        // TerrainRight = TerrainLeft + TerrainWidth;
-        // TerrainTop = TerrainBottom + TerrainLength;
+        TerrainLeft = WorldTerrain.transform.position.x;
+        TerrainBottom = WorldTerrain.transform.position.z;
+        TerrainWidth = WorldTerrain.terrainData.size.x;
+        TerrainLength = WorldTerrain.terrainData.size.z;
+        TerrainHeight = WorldTerrain.terrainData.size.y;
+        TerrainRight = TerrainLeft + TerrainWidth;
+        TerrainTop = TerrainBottom + TerrainLength;
     }
 
     private void Update() {
@@ -58,39 +58,39 @@ public class DestroyCactus : MonoBehaviour {
     //Timer to show coconuts from cactus
     private IEnumerator ShowCoconuts() {
         yield return new WaitForSeconds(3);
-        //InstantiateCertainPosition("Prefabs/Bad_Coconut", 1, 0.5f);
+        InstantiateCertainPosition("Prefabs/Bad_Coconut", 1, 0.5f);
     }
 
 
-    // public void InstantiateCertainPosition(string Resource, int Amount, float AddedHeight) {
+    public void InstantiateCertainPosition(string Resource, int Amount, float AddedHeight) {
 
-    //     var i = 0;
-    //     float terrainHeight = 0f;
-    //     RaycastHit hit;
-    //     float randomPositionX, randomPositionY, randomPositionZ;
-    //     Vector3 randomPosition = Vector3.zero;
+        var i = 0;
+        float terrainHeight = 0f;
+        RaycastHit hit;
+        float randomPositionX, randomPositionY, randomPositionZ;
+        Vector3 randomPosition = Vector3.zero;
 
-    //     do {
-    //         i++;
-    //         randomPositionX = Random.Range (transform.position.x - 2f, transform.position.x + 2f);
-    //         randomPositionZ = Random.Range (transform.position.z - 2f, transform.position.z + 2f);
+        do {
+            i++;
+            randomPositionX = Random.Range (transform.position.x - 2f, transform.position.x + 2f);
+            randomPositionZ = Random.Range (transform.position.z - 2f, transform.position.z + 2f);
 
-    //         if (Physics.Raycast(new Vector3(randomPositionX, 9999f, randomPositionZ), Vector3.down, out hit, Mathf.Infinity, TerrainLayer)) {
-    //             terrainHeight = hit.point.y;
-    //         }
+            if (Physics.Raycast(new Vector3(randomPositionX, 9999f, randomPositionZ), Vector3.down, out hit, Mathf.Infinity, TerrainLayer)) {
+                terrainHeight = hit.point.y;
+            }
 
-    //         if (hit.point.y < 1) {
-    //             terrainHeight = -100;
-    //         }
+            if (hit.point.y < 1) {
+                terrainHeight = -100;
+            }
 
-    //         randomPositionY = terrainHeight + AddedHeight;
+            randomPositionY = terrainHeight + AddedHeight;
 
-    //         randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+            randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
 
-    //         Instantiate(Resources.Load (Resource, typeof(GameObject)), randomPosition, Quaternion.identity);
+            Instantiate(Resources.Load (Resource, typeof(GameObject)), randomPosition, Quaternion.identity);
 
 
-    //     } while (i < Amount);
+        } while (i < Amount);
 
-    // }
+    }
 }
