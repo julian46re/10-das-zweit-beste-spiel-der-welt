5b7acf3bae35e5f01692348e3e5d6cfb7747aae7
felix96s <felix.schmidt1505@gmail.com>
2018-09-13 15:54:54 +0200

    Menu, Health

---
+++ UmaUma_1/Assets/Scripts/DestroyCactus.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyCactus : MonoBehaviour {
+    //Variables
+
+    public Terrain WorldTerrain;
+    public LayerMask TerrainLayer;
+    public static float TerrainLeft, TerrainRight, TerrainBottom, TerrainWidth, TerrainLength, TerrainHeight, TerrainTop;
+
+    public int cactusHealth = 5;
+    private bool isFallen = false;
+    Text TriggerTextCactus;
+    public GameObject cactus;
+    
+    private void Start() {
+    	cactus = transform.gameObject;
+
+        TerrainLeft = WorldTerrain.transform.position.x;
+        TerrainBottom = WorldTerrain.transform.position.z;
+        TerrainWidth = WorldTerrain.terrainData.size.x;
+        TerrainLength = WorldTerrain.terrainData.size.z;
+        TerrainHeight = WorldTerrain.terrainData.size.y;
+        TerrainRight = TerrainLeft + TerrainWidth;
+        TerrainTop = TerrainBottom + TerrainLength;
+    }
+
+    private void Update() {
+        if(cactusHealth > 0 && cactusHealth < 3) {
+            TriggerTextCactus = GameObject.Find("Collision Text").GetComponent<Text>();
+            TriggerTextCactus.enabled = true;
+            TriggerTextCactus.text = "Nochmal schlagen!";
+        } else if (cactusHealth == 0) {
+            TriggerTextCactus.enabled = false;
+        }
+        
+        if(cactusHealth <= 0 && isFallen == false) {
+            Rigidbody rb = cactus.AddComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.AddForce(Vector3.forward, ForceMode.Impulse);
+            StartCoroutine(destroyCactus());
+            isFallen = true;
+            //SHow Coconuts from cactus
+            StartCoroutine(ShowCoconuts());
+        }   
+   
+    }
+
+    private IEnumerator destroyCactus() {
+        yield return new WaitForSeconds(5);
+        //thisTree.active = false;
+        Destroy(cactus);
+    }
+    
+    //Timer to show coconuts from cactus
+    private IEnumerator ShowCoconuts() {
+        yield return new WaitForSeconds(3);
+        InstantiateCertainPosition("Prefabs/Bad_Coconut", 1, 0.5f);
+    }
+
+
+    public void InstantiateCertainPosition(string Resource, int Amount, float AddedHeight) {
+
+        var i = 0;
+        float terrainHeight = 0f;
+        RaycastHit hit;
+        float randomPositionX, randomPositionY, randomPositionZ;
+        Vector3 randomPosition = Vector3.zero;
+
+        do {
+            i++;
+            randomPositionX = Random.Range (transform.position.x - 2f, transform.position.x + 2f);
+            randomPositionZ = Random.Range (transform.position.z - 2f, transform.position.z + 2f);
+
+            if (Physics.Raycast(new Vector3(randomPositionX, 9999f, randomPositionZ), Vector3.down, out hit, Mathf.Infinity, TerrainLayer)) {
+                terrainHeight = hit.point.y;
+            }
+
+            if (hit.point.y < 1) {
+                terrainHeight = -100;
+            }
+
+            randomPositionY = terrainHeight + AddedHeight;
+
+            randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+
+            Instantiate(Resources.Load (Resource, typeof(GameObject)), randomPosition, Quaternion.identity);
+
+
+        } while (i < Amount);
+
+    }
+}
---
+++ UmaUma_1/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+
+			if (GameIsPaused) {
+
+				Resume();
+
+			} else {
+
+				Pause();
+
+			}
+
+		}
+
+	}
+
+	void Resume() {
+
+		//GetComponent<camControl>.enabled = true;
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+
+	}
+
+	void Pause() {
+
+		//GetComponent<camControl>.enabled = false;
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+}
+
